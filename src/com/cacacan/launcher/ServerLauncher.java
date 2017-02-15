8214804671c4e8b0f2cc8d39c49109101d858756
Sergio Flores <vitavitae1@gmail.com>
2017-02-15 22:25:38 +0100

    Created as maven project, specified dependencies in pom

--- src/com/cacacan/launcher/ServerLauncher.java
+++ src/com/cacacan/launcher/ServerLauncher.java
@@ -1,20 +1,11 @@
 package com.cacacan.launcher;
 
 import com.cacacan.receiver.LocationGetterHTTPServerRunnable;
-import com.cacacan.sender.GcmSenderRunnable;
 
 public class ServerLauncher {
 
 	public static void main(String[] args) {
 		final Thread receivingThread = new Thread(new LocationGetterHTTPServerRunnable());
-
-		final GcmSenderRunnable sendingRunnable = new GcmSenderRunnable();
-		sendingRunnable.setMessage("HOLA HOLA HOLA");
-		final Thread sendingThread = new Thread(sendingRunnable);
-
 		receivingThread.start();
-		if (false) {
-			sendingThread.start();
-		}
 	}
 }
